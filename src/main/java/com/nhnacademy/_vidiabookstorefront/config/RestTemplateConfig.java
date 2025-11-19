c8f5ad0270499c132ba92ffad28dd12db07eae84
BaeJongOk <0511jongok@gmail.com>
2025-11-20 08:52:47 +0900

    완성?

--- src/main/java/com/nhnacademy/_vidiabookstorefront/config/RestTemplateConfig.java
+++ src/main/java/com/nhnacademy/_vidiabookstorefront/config/RestTemplateConfig.java
@@ -1,19 +1,17 @@
 package com.nhnacademy._vidiabookstorefront.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
-                .setConnectTimeout(Duration.ofSeconds(5))
-                .setReadTimeout(Duration.ofSeconds(5))
                 .build();
     }
 }
